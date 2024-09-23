50489bcff9bf815e4bf60c35df91789dabf92d8f
sofia <sofi.ruizsuarez@gmail.com>
2024-09-23 15:24:38 -0400

    documentation file

---
+++ R/documentation.R
@@ -0,0 +1,26 @@
+#' Brazil Boundaries
+#'
+#' This dataset contains the boundaries of Brazil's states.
+#'
+#' @docType data
+#' @usage data(brazil_limits)
+#' @format A `SpatialPolygonsDataFrame`
+#' @source Generated from geodata library.
+"brazil_limits"
+
+
+#' Brazil Boundaries
+#'
+#' This dataset contains all the mortality of women between 10 and 49 years old
+#'  occurred between 2007-2010 at municipality level.
+#'  The ICD10 codes considered were: X85-Y09 and Y10-Y34.
+#'
+#' @docType data
+#' @usage data(femicides_data)
+#' @format A ` "sf"         "data.frame"`
+#' @source Raw data was obtained from Sistema de Informações de Mortalidade (SIM)
+#' The ICD10 codes considered were: X85-Y09 and Y10-Y34.
+ "mortality_data"
+
+
+
